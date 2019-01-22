e543e5dcb5ca5803d3cfcded8554449ebf5a7121
lilvina <davinaleo2007@yahoo.com>
2019-01-22 13:42:59 -0800

    finished callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,11 +1,13 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+function callback(value) {
+  console.log(value)
+}
+/*
 
-/* 
+  //Given this problem:
 
-  //Given this problem: 
-  
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -24,24 +26,41 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
+getLength(items,callback)
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[3])
 }
+last(items, callback)
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
+sumNums(2, 5, callback)
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y)
 }
+multiplyNums(5, 5, callback)
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  list.find((element) => {
+    if(element === item) {
+      return cb(true)
+    } else {
+      return cb(false)
+    }
+  })
 }
+contains("Davina", items, callback)
 
 /* STRETCH PROBLEM */
 
